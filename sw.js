ba40e9aec4f42be6aed01ea069fddf4b6c3619ff
star-plucking <1328638004@qq.com>
2026-01-26 22:20:21 +0800

    Site updated: 2026-01-26 22:20:20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1769436464438";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1769437198239";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
